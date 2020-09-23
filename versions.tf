d8e4bfee39b9837e4c6cb5fb025c4c93375f7dcc
Dereck Zenda <dereck.zenda@gmail.com>
2020-09-23 02:28:33 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,19 +1,1 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 2.7.0"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-  required_version = ">= 0.12"
-}
 
-provider "azurerm" {
-  features {}
-}
